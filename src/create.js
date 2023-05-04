3fea4f1968ae187063d0d6d47f15bb775365a835
leexiaohui <1287338537@qq.com>
2023-05-04 15:14:25 +0800

    init: store封装

--- src/create.js
+++ src/create.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import loadComponent from './auto/component';
 
 export default function create() {
   return createApp(App)
+    .use(createPinia())
     .use(router)
     .use(loadComponent);
 }
